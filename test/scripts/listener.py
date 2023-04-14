8da9c33f4974cde132fe53f0b30ba12885d1ff6b
cas101101 <caleb.a.schwartz@gmail.com>
2023-04-14 10:01:40 -0400

    Updated to do tests with pub/sub

--- test/scripts/listener.py
+++ test/scripts/listener.py
@@ -2,26 +2,43 @@
 
 import rospy
 from std_msgs.msg import String
+import pi_servo_hat
+import time
 
 heading = 360
+
+def init():
+    # Initialize Constructor
+    test = pi_servo_hat.PiServoHat()
+
+    # Restart Servo Hat (in case Hat is frozen/locked)
+    test.restart()
+
+    # Test Run
+    #########################################
+    # Moves servo position to 0 degrees (1ms), Channel 0
+    test.move_servo_position(0, 0)
+
+    # Pause 1 sec
+    time.sleep(3)
+
+    # Moves servo position to 90 degrees (2ms), Channel 0
+    test.move_servo_position(0, 90)
+
 
 def callback(data):
     
     global heading
     
     if( data.data == 'left' ):
-        
-        if( heading == 0 ):
-            heading = 360
-        
-        heading -= 15
+        x = 'heard left'
+        print(x)
+        # Turn left code
      
     if( data.data == 'right' ): 
-        
-        if( heading == 360 ):
-            heading = 0
-        
-        heading += 15
+        x = 'heard right'
+        print(x)
+        # Turn right code
         
     rospy.loginfo( 'heading %d', heading )
     
@@ -32,9 +49,11 @@ def listener():
 
     rospy.init_node('listener', anonymous=True)
 
-    rospy.Subscriber('direction', String, callback)
+    rospy.Subscriber('direction', String, callback) #subscribed to direction
+
 
     rospy.spin()
 
 if __name__ == '__main__':
+    init()
     listener()
